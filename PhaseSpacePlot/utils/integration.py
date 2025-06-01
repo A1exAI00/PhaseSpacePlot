06aa35fd494b6fc929761513ee258f240f21bbc1
Alex Akinin <lksndrakinin@gmail.com>
2025-06-01 20:36:33 +0300

    Move scipy integration into a Trajectory method

--- PhaseSpacePlot/utils/integration.py
+++ PhaseSpacePlot/utils/integration.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+
 def euler_integrate(ODEs, U0, p, t, dt):
     sol = []
     t_sol = []
@@ -12,27 +15,27 @@ def euler_integrate(ODEs, U0, p, t, dt):
         curr_t += dt
     return (sol, t_sol)
 
-def scipy_integrate(ODEs, U0, p, t_start, t_end, dt):
-    import numpy as np
-    from scipy.integrate import solve_ivp
-
-    sol = solve_ivp(lambda t, U0: ODEs(U0, np.array(p), t), 
-                    t_span=(t_start, t_end), 
-                    y0=U0, 
-                    t_eval=np.arange(t_start, t_end, dt), 
-                    method="RK45", 
-                    rtol = 1e-5, atol = 1e-5)
-    return (sol.y, sol.t)
-
 #########################################################################################
 
 class Trajectory():
-    def __init__(self, init_state):
-        self.init_state = init_state
+    def __init__(self):
         self.sol = None
         self.t_sol = None
+        self.alg = "RK45"
+        self.rtol = 1e-5
+        self.atol = 1e-5
+        return
     
-    def integrate_scipy(self, ODEs, pars, t_start, t_end, t_N):
-        sol, t_sol = scipy_integrate(ODEs, self.init_state, pars, t_start, t_end, (t_end-t_start)/t_N)
-        self.sol = sol
-        self.t_sol = t_sol
+    def integrate_scipy(self, ODEs, init_state, pars, t_start, t_end, t_N):
+        t_span = (t_start, t_end)
+        t_eval = np.arange(t_start, t_end, (t_end-t_start)/t_N)
+        sol = solve_ivp(lambda t, U0: ODEs(U0, np.array(pars), t), 
+                    t_span=t_span, 
+                    y0=init_state, 
+                    t_eval=t_eval, 
+                    method=self.alg, 
+                    rtol=self.rtol, 
+                    atol=self.atol)
+        self.sol = sol.y
+        self.t_sol = sol.t
+        return
