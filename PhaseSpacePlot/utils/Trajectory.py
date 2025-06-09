e99331e437e5d730fc243c0140fa3cb80b2d344d
Alex Akinin <lksndrakinin@gmail.com>
2025-06-09 17:07:00 +0300

    Refactor Trajectory class

--- PhaseSpacePlot/utils/Trajectory.py
+++ PhaseSpacePlot/utils/Trajectory.py
@@ -1,22 +1,6 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 
-def euler_integrate(ODEs, U0, p, t, dt):
-    sol = []
-    t_sol = []
-    sol.append(U0)
-    curr_t = 0.0
-    t_sol.append(0.0)
-    while curr_t < t:
-        dU = ODEs(sol[-1], p, t)
-        dU = [_dU*dt for _dU in dU]
-        sol.append([sol[-1][i]+dU[i] for i in range(len(U0))])
-        t_sol.append(t_sol[-1]+dt)
-        curr_t += dt
-    return (sol, t_sol)
-
-#########################################################################################
-
 class Trajectory():
     def __init__(self):
         self.sol = None
