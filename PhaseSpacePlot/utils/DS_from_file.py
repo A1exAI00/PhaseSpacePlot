ad3d2f8d5c57fedf4131db1b69fe45ede7f40ab4
Alex Akinin <lksndrakinin@gmail.com>
2025-06-09 17:49:27 +0300

    Remove unwanted docstring

--- PhaseSpacePlot/utils/DS_from_file.py
+++ PhaseSpacePlot/utils/DS_from_file.py
@@ -1,8 +1,4 @@
 def import_module_with_exec(file_path):
-    """
-    Import a module by executing the file directly.
-    Note: This approach is less secure than importlib.
-    """
     with open(file_path, 'r') as f:
         code = compile(f.read(), file_path, 'exec')
         module = {}
